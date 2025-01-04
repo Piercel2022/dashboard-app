a16c81b110cfb5d413ded010cf9c5fbbd263e32b
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-05 00:45:54 +0100

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>My Dashboard</h1>
+            <div className="user-info">Welcome, User</div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h2>Dashboard</h2>
+            <ul>
+                <li>Home</li>
+                <li>Analytics</li>
+                <li>Settings</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
